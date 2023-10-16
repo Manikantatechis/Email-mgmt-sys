4f15cab07128d6cd9a1d50c8c004878ab61aadd4
Mani <manikanta@techis.io>
2023-10-16 20:42:07 +0530

    implemented total sms and email report

--- controllers/reportsController.js
+++ controllers/reportsController.js
@@ -2,12 +2,10 @@ const EmailBatch = require("../models/emailModel");
 const asyncHandler = require("express-async-handler");
 const kixieBatch = require("../models/kixieModel");
 
-const getEmailData = 
-asyncHandler(async (req, res) => {
+const getEmailData = asyncHandler(async (req, res) => {
   try {
-    // req.query.slot = "week"
     const slot = req.query.slot || "month";
-    console.log(slot)
+    console.log(slot);
 
     const responseData = {
       emailSent: slot === "month" ? Array(12).fill(0) : Array(7).fill(0),
@@ -46,6 +44,28 @@ asyncHandler(async (req, res) => {
       responseData.emailOpened[index] = data.totalEmailsOpened;
     });
 
+    // New aggregation pipeline for total emails sent and opened till date
+    const totalAggregatedData = await EmailBatch.aggregate([
+      {
+        $group: {
+          _id: null,
+          totalEmailsSentAllTime: { $sum: "$emailCount" },
+          totalEmailsOpenedAllTime: { $sum: { $size: "$openedEmail" } },
+        },
+      },
+    ]);
+
+    // Adding the total counts to responseData
+    if (totalAggregatedData[0]) {
+      responseData.totalEmailsSentAllTime =
+        totalAggregatedData[0].totalEmailsSentAllTime;
+      responseData.totalEmailsOpenedAllTime =
+        totalAggregatedData[0].totalEmailsOpenedAllTime;
+    } else {
+      responseData.totalEmailsSentAllTime = 0;
+      responseData.totalEmailsOpenedAllTime = 0;
+    }
+
     res.status(200).json(responseData);
   } catch (error) {
     res
@@ -54,50 +74,57 @@ asyncHandler(async (req, res) => {
   }
 });
 
-const getSMSData = asyncHandler(
+const getSMSData = asyncHandler(async (req, res) => {
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let today = new Date();
+  let weekData = new Array(7).fill(0);
 
-async (req, res) => {
-    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    let today = new Date();
-    let weekData = new Array(7).fill(0);
+  // Determine the start of the 7-day period
+  let startOfPeriod = new Date(today);
+  startOfPeriod.setDate(today.getDate() - 6);
+  startOfPeriod.setHours(0, 0, 0, 0);
 
-    for (let i = 0; i < 7; i++) {
-        let startOfDay = new Date(today);
-        startOfDay.setHours(0, 0, 0, 0);
+  // Aggregate data for the entire 7-day period
+  let aggregatedData = await kixieBatch.aggregate([
+      {
+          $match: {
+              timestamp: {
+                  $gte: startOfPeriod,
+                  $lte: today,
+              },
+          },
+      },
+      {
+          $group: {
+              _id: {
+                  dayOfWeek: { $dayOfWeek: "$timestamp" },
+              },
+              totalSMS: { $sum: "$smsCount" },
+          },
+      },
+  ]);
 
-        let endOfDay = new Date(today);
-        endOfDay.setHours(23, 59, 59, 999);
+  // Map the aggregated data to the weekData array
+  for (let data of aggregatedData) {
+      const index = (data._id.dayOfWeek + 5) % 7; // MongoDB's dayOfWeek is 1-indexed starting from Sunday
+      weekData[index] = data.totalSMS;
+  }
 
-        let aggregatedData = await kixieBatch.aggregate([
-            {
-                $match: {
-                    timestamp: {
-                        $gte: startOfDay,
-                        $lte: endOfDay
-                    }
-                }
-            },
-            {
-                $group: {
-                    _id: null,
-                    totalSMS: { $sum: "$smsCount" }
-                }
-            }
-        ]);
+  // Aggregate total SMS sent all time
+  const totalAggregatedData = await kixieBatch.aggregate([
+      {
+          $group: {
+              _id: null,
+              totalSMSSentAllTime: { $sum: "$smsCount" },
+          },
+      },
+  ]);
 
-        let count = aggregatedData.length ? aggregatedData[0].totalSMS : 0;
-
-        weekData[daysOfWeek.indexOf(daysOfWeek[today.getDay()])] = count;
-
-        // Move to previous day
-        today.setDate(today.getDate() - 1);
-    }
-
-    res.json(weekData);
+  res.json({
+      weekData,
+      totalSMSSentAllTime: totalAggregatedData[0].totalSMSSentAllTime
+  });
 });
 
 
-
-
-
-module.exports = {getEmailData, getSMSData};
+module.exports = { getEmailData, getSMSData };

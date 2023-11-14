71bc2cd432a39f88f5fbb8cbecb63adea7145338
Mani <manikanta@techis.io>
2023-11-14 14:18:14 +0530

    updated controller fn for email Report

--- controllers/reportsController.js
+++ controllers/reportsController.js
@@ -3,14 +3,18 @@ const asyncHandler = require("express-async-handler");
 const kixieBatch = require("../models/kixieModel");
 
 const getEmailData = asyncHandler(async (req, res) => {
-  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  console.log(req.query)
+  const {slot} = req.query
+  const reportType = req.query.slot || "week"
+
   let today = new Date();
   let weekData = {
     emailSent: new Array(7).fill(0),
     emailOpened: new Array(7).fill(0),
   };
-
-  // Determine the start of the 7-day period for current week and past week
+  if (reportType === "week") {
+// Determine the start of the 7-day period for current week and past week
   let startOfCurrentPeriod = new Date(today);
   startOfCurrentPeriod.setDate(today.getDate() - today.getDay()); // This will give us start of this week (Sunday)
   startOfCurrentPeriod.setHours(0, 0, 0, 0);
@@ -59,21 +63,26 @@ const getEmailData = asyncHandler(async (req, res) => {
     },
   ]);
 
-  // Map the aggregated data to the weekData array
   for (let data of aggregatedDataCurrentWeek) {
-    const index = (data._id.dayOfWeek + 5) % 7; // Shift to align with our array
+    const index = (data._id.dayOfWeek + 5) % 7; // New index adjustment
     weekData.emailSent[index] = data.totalEmailsSent;
     weekData.emailOpened[index] = data.totalEmailsOpened;
   }
-
-  for (let i = today.getDay() + 1; i < 7; i++) {
+  
+  // Handle data for the next day(s) from the past week
+  const currentDayIndex = (today.getDay() + 6) % 7; // Adjust current day index
+  for (let i = currentDayIndex + 1; i < 7; i++) {
     const data = aggregatedDataPastWeek.find(d => (d._id.dayOfWeek + 5) % 7 === i);
     if (data) {
       weekData.emailSent[i] = data.totalEmailsSent;
       weekData.emailOpened[i] = data.totalEmailsOpened;
+    } else {
+      weekData.emailSent[i] = 0;
+      weekData.emailOpened[i] = 0;
     }
   }
-
+  
+  
   // Aggregate total emails sent and opened all time
   const totalAggregatedData = await EmailBatch.aggregate([
     {
@@ -95,6 +104,74 @@ const getEmailData = asyncHandler(async (req, res) => {
   }
 
   res.status(200).json(weekData);
+  }
+
+  if (reportType === "month") {
+
+    
+    console.log(slot);
+
+    const responseData = {
+      emailSent:  Array(12).fill(0),
+      emailOpened: Array(12).fill(0) 
+    };
+
+    const groupBy =
+      slot === "month"
+        ? { year: { $year: "$timestamp" }, month: { $month: "$timestamp" } }
+        : {
+            year: { $year: "$timestamp" },
+            week: { $isoDayOfWeek: "$timestamp" },
+          };
+
+    const sortCriteria ={ "_id.year": 1, "_id.month": 1 }
+       
+
+    const aggregatedData = await EmailBatch.aggregate([
+      {
+        $group: {
+          _id: groupBy,
+          totalEmailsSent: { $sum: "$emailCount" },
+          totalEmailsOpened: { $sum: { $size: "$openedEmail" } },
+        },
+      },
+      {
+        $sort: sortCriteria,
+      },
+    ]);
+
+    aggregatedData.forEach((data) => {
+      const index = slot === "month" ? data._id.month - 1 : data._id.week - 1;
+      responseData.emailSent[index] = data.totalEmailsSent;
+      responseData.emailOpened[index] = data.totalEmailsOpened;
+    });
+
+    // New aggregation pipeline for total emails sent and opened till date
+    const totalAggregatedData = await EmailBatch.aggregate([
+      {
+        $group: {
+          _id: null,
+          totalEmailsSentAllTime: { $sum: "$emailCount" },
+          totalEmailsOpenedAllTime: { $sum: { $size: "$openedEmail" } },
+        },
+      },
+    ]);
+
+    // Adding the total counts to responseData
+    if (totalAggregatedData[0]) {
+      responseData.totalEmailsSentAllTime =
+        totalAggregatedData[0].totalEmailsSentAllTime;
+      responseData.totalEmailsOpenedAllTime =
+        totalAggregatedData[0].totalEmailsOpenedAllTime;
+    } else {
+      responseData.totalEmailsSentAllTime = 0;
+      responseData.totalEmailsOpenedAllTime = 0;
+    }
+
+    res.status(200).json(responseData);
+  
+  }
+  
 });
 
 
